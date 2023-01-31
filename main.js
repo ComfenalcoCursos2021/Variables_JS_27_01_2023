8ae2935dc6a5759970ce2b9b79b091c0739e1541
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2023-01-30 19:43:36 -0500

    Mostrar datos en la consola

--- main.js
+++ main.js
@@ -1,32 +1,22 @@
 
 
+let nombre = "Miguel Angel Castro Escamilla";
 
+console.log(nombre);
+console.warn(nombre);
+console.error(nombre);
+console.info(nombre);
 
-// String Cadenas de texto.
-// Number Valores numéricos.
-// Boolean Representa una entidad lógica y puede tener dos valores: true y false.
-// Object Un valor en memoria al que podemos acceder por un identificador.
-
-let nombre = "Miguel";
-let altura = 1.63;
-let soyAprendiz = false;
-let curiculo = {
-    secundaria: "Bachiller tecnico especialista en informatica",
-    experiencia_web: "Full stact en vue",
-    ingenieria: "Software",
-    años_maestria: 2
-}
-
-console.log(`El nombre es ${nombre}`);
-console.log(`La altura es ${altura}`);
-console.log(`El rol aprendiz es ${soyAprendiz}`);
-
-console.groupCollapsed("Curriculo")
-    console.table([curiculo])
+console.group("Mensaje de usuario");
+    console.log("Mensajes agrupados con titulo");
 console.groupEnd();
 
+console.table([nombre]);
+console.table([{nombre_variable: nombre}]);
 
+// console.clear();
 
-
-
+console.time();
+    console.log("Tiempo que se demoro la web en cargar este mensaje");
+console.timeEnd();
 
